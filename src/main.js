52c5b8b0179a83dde1a1a2b6f705ddeede921779
wenn <tatianacharry95@gmail.com>
2024-06-23 18:17:33 -0500

    Primer respositorio de WENDAY

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import './assets/main.css'
+import vuetify from './vuetify'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-btn @click="saludar">¡Hola, Vuetify!</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script>
+export default {
+  methods: {
+    saludar() {
+      alert('¡Hola, Vuetify!')
+    }
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
